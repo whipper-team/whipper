a696b14ffe87a46ec88ac63502735d9aac935514
Thomas Vander Stichele <thomas (at) apestaart (dot) org>
2009-04-11 22:08:29 +0000

    * morituri/test/test_image_image.py (added): 	  Add tests for AudioRip CRC'ing images.

---
+++ morituri/test/test_image_image.py
@@ -0,0 +1,48 @@
+# -*- Mode: Python; test-case-name: morituri.test.test_image_cue -*-
+# vi:si:et:sw=4:sts=4:ts=4
+
+import os
+import unittest
+
+import gobject
+gobject.threads_init()
+
+from morituri.image import image
+from morituri.common import task
+
+def h(i):
+    return "0x%08x" % i
+
+class TrackSingleTestCase(unittest.TestCase):
+    def setUp(self):
+        self.image = image.Image(os.path.join(os.path.dirname(__file__),
+            'track-single.cue'))
+
+    def testAudioRipCRC(self):
+        crctask = image.AudioRipCRCTask(self.image) 
+        runner = task.SyncRunner()
+        runner.run(crctask, verbose=False)
+
+        self.assertEquals(len(crctask.crcs), 4)
+        self.assertEquals(h(crctask.crcs[0]), '0x00000000')
+        self.assertEquals(h(crctask.crcs[1]), '0x793fa868')
+        self.assertEquals(h(crctask.crcs[2]), '0x8dd37c26')
+        self.assertEquals(h(crctask.crcs[3]), '0x00000000')
+
+class KingsSeparateTestCase(unittest.TestCase):
+    def setUp(self):
+        self.image = image.Image(os.path.join(os.path.dirname(__file__),
+            'track-separate.cue'))
+
+    def testAudioRipCRC(self):
+        crctask = image.AudioRipCRCTask(self.image) 
+        runner = task.SyncRunner()
+        runner.run(crctask, verbose=False)
+
+        self.assertEquals(len(crctask.crcs), 4)
+        self.assertEquals(h(crctask.crcs[0]), '0xaf18681e')
+        self.assertEquals(h(crctask.crcs[1]), '0xd63dc2d2')
+        self.assertEquals(h(crctask.crcs[2]), '0xd63dc2d2')
+        self.assertEquals(h(crctask.crcs[3]), '0x7271db39')
+
+
