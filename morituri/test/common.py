316eefbbd987cf1d4d4b4a0b54ef85c56b261e9b
Thomas Vander Stichele <thomas (at) apestaart (dot) org>
2009-06-04 09:03:45 +0000

    * morituri/test/common.py: 	  Add a method for diffing multiline strings.

--- morituri/test/common.py
+++ morituri/test/common.py
@@ -4,3 +4,29 @@
 from morituri.common import log
 
 log.init()
+
+# lifted from flumotion
+
+def _diff(old, new, desc):
+    import difflib
+    lines = difflib.unified_diff(old, new)
+    lines = list(lines)
+    if not lines:
+        return
+    output = ''
+    for line in lines:
+        output += '%s: %s\n' % (desc, line[:-1])
+
+    raise AssertionError(
+        ("\nError while comparing strings:\n"
+         "%s") % (output, ))
+
+
+def diffStrings(orig, new, desc='input'):
+
+    def _tolines(s):
+        return [line + '\n' for line in s.split('\n')]
+
+    return _diff(_tolines(orig),
+                 _tolines(new),
+                 desc=desc)
