9a26d7f6a482893a990f3edba4622fb497a4755b
Thomas Vander Stichele <thomas (at) apestaart (dot) org>
2009-06-05 07:57:54 +0000

    * configure.ac: 	* morituri/Makefile.am: 	* morituri/result (added): 	* morituri/result/result.py (added): 	* morituri/result/__init__.py (added): 	* morituri/result/Makefile.am (added): 	  Add classes to store track and rip results in.

---
+++ morituri/result/result.py
@@ -0,0 +1,60 @@
+# -*- Mode: Python; test-case-name: morituri.test.test_result_result -*-
+# vi:si:et:sw=4:sts=4:ts=4
+
+# Morituri - for those about to RIP
+
+# Copyright (C) 2009 Thomas Vander Stichele
+
+# This file is part of morituri.
+# 
+# morituri is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# morituri is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with morituri.  If not, see <http://www.gnu.org/licenses/>.
+
+class TrackResult:
+    """
+    @ivar testcrc: 4-byte CRC for the test read
+    @type testcrc: int
+    @ivar copycrc: 4-byte CRC for the copy read
+    @type copycrc: int
+    """
+    number = None
+    filename = None
+    pregap = 0 # in frames
+
+    peak = 0.0
+    quality = 0.0
+    testcrc = None
+    copycrc = None
+    accurip = False # whether it's in the database
+    accuripCRC = None
+    accuripDatabaseCRC = None
+    accuripDatabaseConfidence = None
+
+class RipResult:
+    """
+    I hold information about the result for rips.
+    I can be used to write log files.
+
+    @ivar offset: sample read offset
+    """
+
+    offset = 0
+    toctable = None
+    artist = None
+    title = None
+
+    vendor = None
+    model = None
+
+    def __init__(self):
+        self.tracks = []
