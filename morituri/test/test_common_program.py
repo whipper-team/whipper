ef69de48baea56691af59ea00eaa3bed37aea7e7
Thomas Vander Stichele <thomas (at) apestaart (dot) org>
2011-10-19 18:00:36 +0000

    * morituri/common/Makefile.am: 	* morituri/common/musicbrainz.py: 	* morituri/common/program.py: 	* morituri/test/test_common_program.py: 	* morituri/test/Makefile.am: 	* morituri/test/test_common_musicbrainz.py (added): 	* morituri/common/musicbrainz.py (added): 	  Split off musicbrainz-specific code.

--- morituri/test/test_common_program.py
+++ morituri/test/test_common_program.py
@@ -7,7 +7,7 @@ import pickle
 import unittest
 
 from morituri.result import result
-from morituri.common import program, accurip
+from morituri.common import program, accurip, musicbrainz
 from morituri.rip import cd
 
 class TrackImageVerifyTestCase(unittest.TestCase):
@@ -90,7 +90,7 @@ class PathTestCase(unittest.TestCase):
         
     def testStandardTemplateFilled(self):
         prog = program.Program()
-        md = program.DiscMetadata()
+        md = musicbrainz.DiscMetadata()
         md.artist = md.sortName = 'Jeff Buckley'
         md.title = 'Grace'
         prog.metadata = md
@@ -101,7 +101,7 @@ class PathTestCase(unittest.TestCase):
 
     def testIssue66TemplateFilled(self):
         prog = program.Program()
-        md = program.DiscMetadata()
+        md = musicbrainz.DiscMetadata()
         md.artist = md.sortName = 'Jeff Buckley'
         md.title = 'Grace'
         prog.metadata = md
@@ -109,48 +109,3 @@ class PathTestCase(unittest.TestCase):
         path = prog.getPath(u'/tmp', u'%A/%d', 'mbdiscid', 0)
         self.assertEquals(path,
             u'/tmp/Jeff Buckley/Grace')
-  
-class MetadataLengthTestCase(unittest.TestCase):
-    def testLamprey(self):
-        from musicbrainz2 import wsxml
-
-        path = os.path.join(os.path.dirname(__file__),
-            'release.c7d919f4-3ea0-4c4b-a230-b3605f069440.xml')
-        handle = open(path, "rb")
-
-        reader = wsxml.MbXmlParser()
-        wsMetadata = reader.parse(handle)
-        release = wsMetadata.getRelease()
-        metadata = program.getMetadata(release)
-
-        self.assertEquals(metadata.duration, 2962889)
-
-    def testLadyhawke(self):
-        from musicbrainz2 import wsxml
-
-        path = os.path.join(os.path.dirname(__file__),
-            'release.93a6268c-ddf1-4898-bf93-fb862b1c5c5e.xml')
-        handle = open(path, "rb")
-
-        reader = wsxml.MbXmlParser()
-        wsMetadata = reader.parse(handle)
-        release = wsMetadata.getRelease()
-        metadata = program.getMetadata(release)
-        self.failUnless(metadata)
-
-        # self.assertEquals(metadata.duration, 2609413)
-
-    def testDasCapital(self):
-        from musicbrainz2 import wsxml
-
-        path = os.path.join(os.path.dirname(__file__),
-            'release.08397059-86c1-463b-8ed0-cd596dbd174f.xml')
-        handle = open(path, "rb")
-
-        reader = wsxml.MbXmlParser()
-        wsMetadata = reader.parse(handle)
-        release = wsMetadata.getRelease()
-        metadata = program.getMetadata(release)
-
-        # FIXME: 2 seconds longer than the duration according to table
-        self.assertEquals(metadata.duration, 2315730)
