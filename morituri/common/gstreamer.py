a3fe7ba7e7cd97a4c80e6c5376e547b158ef7d00
Thomas Vander Stichele <thomas (at) apestaart (dot) org>
2012-11-10 21:57:06 +0000

    pep8 fixes

--- morituri/common/gstreamer.py
+++ morituri/common/gstreamer.py
@@ -6,23 +6,25 @@
 # Copyright (C) 2009 Thomas Vander Stichele
 
 # This file is part of morituri.
-# 
+#
 # morituri is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # morituri is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with morituri.  If not, see <http://www.gnu.org/licenses/>.
 
 from morituri.common import log
 
 # workaround for issue #64
+
+
 def removeAudioParsers():
     log.debug('gstreamer', 'Removing buggy audioparsers plugin if needed')
 
