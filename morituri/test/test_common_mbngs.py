7533b269fc8f530f187fdc0472df9e7bab4e95bc
Thomas Vander Stichele <thomas (at) apestaart (dot) org>
2013-07-30 12:47:00 +0200

    add another test to fix

--- morituri/test/test_common_mbngs.py
+++ morituri/test/test_common_mbngs.py
@@ -49,3 +49,29 @@ class MetadataTestCase(unittest.TestCase):
         #        of track artist id's
         self.assertEquals(track16.mbidArtist,
             u'89ad4ac3-39f7-470e-963a-56509c546377')
+
+    def testBalladOfTheBrokenSeas(self):
+        # various artists disc
+        path = os.path.join(os.path.dirname(__file__),
+            'morituri.release.e32ae79a-336e-4d33-945c-8c5e8206dbd3.json')
+        handle = open(path, "rb")
+        response = json.loads(handle.read())
+        handle.close()
+        discid = "xAq8L4ELMW14.6wI6tt7QAcxiDI-"
+
+        metadata = mbngs._getMetadata({}, response['release'], discid)
+
+        self.assertEquals(metadata.artist, u'Isobel Campbell & Mark Lanegan')
+        self.assertEquals(metadata.release, u'2006-01-30')
+        # FIXME: this is only Isobel
+        self.assertEquals(metadata.mbidArtist,
+            u'd51f3a15-12a2-41a0-acfa-33b5eae71164')
+
+        self.assertEquals(len(metadata.tracks), 12)
+
+        track12 = metadata.tracks[11]
+
+        self.assertEquals(track12.artist, u'Isobel Campbell & Mark Lanegan')
+        # FIXME: this is only Isobel
+        self.assertEquals(track12.mbidArtist,
+            u'd51f3a15-12a2-41a0-acfa-33b5eae71164')
