ae3d4da2e5dbe95b373ae65806cb4836e2d9811f
JoeLametta <JoeLametta@users.noreply.github.com>
2016-07-27 12:09:16 +0200

    Really fix 9099da2
    Too many parentheses: I got confused
    This one should finally solve:
    
    ```python
    TypeError: unsupported operand type(s) for %: 'NoneType' and 'tuple'
    ```

--- morituri/result/logger.py
+++ morituri/result/logger.py
@@ -119,8 +119,8 @@ class MorituriLogger(result.Logger):
             elif self._accuratelyRipped < nonHTOA:
                 accurateTracks = nonHTOA - self._accuratelyRipped
                 lines.append("%s Some tracks could not be verified as "
-                             "accurate (%d/%d got no match)") % (
-                             arHeading, accurateTracks, nonHTOA)
+                             "accurate (%d/%d got no match)" % (
+                             arHeading, accurateTracks, nonHTOA))
             else:
                 lines.append("%s All tracks accurately ripped" % arHeading)
 
