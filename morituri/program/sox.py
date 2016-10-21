4ccc981a6b5a359d2388c7f15b48a15d78cef7a8
Samantha Baldwin <fuhsaz+git@cryptic.li>
2016-10-21 13:38:51 -0400

    use .splitlines() instead of .split('n')

--- morituri/program/sox.py
+++ morituri/program/sox.py
@@ -21,4 +21,4 @@ def peak_level(track_path):
         return None
     # relevant captured line looks like:
     # Maximum amplitude: 0.123456
-    return float(err.split('\n')[3].split()[2])
+    return float(err.splitlines()[3].split()[2])
