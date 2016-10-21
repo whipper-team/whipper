0748a29606270570d84686bb070da66717f05354
Samantha Baldwin <fuhsaz+git@cryptic.li>
2016-10-21 00:52:46 -0400

    standardise program/sox.py formatting, add test case

--- morituri/program/sox.py
+++ morituri/program/sox.py
@@ -1,16 +1,18 @@
-import os
 import logging
+import os
 from subprocess import Popen, PIPE
 
 SOX = 'sox'
 
 def peak_level(track_path):
-	if not os.path.exists(track_path):
-		logging.warning("SoX peak detection failed: file not found")
-		return None
-	sox = Popen([SOX, track_path, "-n", "stat"], stderr=PIPE)
-	out, err = sox.communicate()
-	if sox.returncode:
-		logging.warning("SoX peak detection failed: " + s.returncode)
-		return None
-	return float(err.split('\n')[3].split()[2])	# Maximum amplitude: 0.123456
+    if not os.path.exists(track_path):
+        logging.warning("SoX peak detection failed: file not found")
+        return None
+    sox = Popen([SOX, track_path, "-n", "stat"], stderr=PIPE)
+    out, err = sox.communicate()
+    if sox.returncode:
+        logging.warning("SoX peak detection failed: " + str(sox.returncode))
+        return None
+    # relevant captured line looks like:
+    # Maximum amplitude: 0.123456
+    return float(err.split('\n')[3].split()[2])
