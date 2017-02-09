whipper-team/whipper

commit fb271f08cdee877795091065c344dcc902d1dcbf

Add pre-emphasis status reporting to whipper's logfiles Fixes #120

morituri/result/logger.py
@@ -25,9 +25,8 @@ class MorituriLogger(result.Logger):
         lines = []
 
         # Ripper version
-        # Only implemented in whipper (ripResult.logger)
-        lines.append("Log created by: whipper %s (%s logger)" % (
-                    morituri.__version__, ripResult.logger))
+        lines.append("Log created by: whipper %s (internal logger)" %
+                     morituri.__version__)
 
         # Rip date
         date = time.strftime("%Y-%m-%dT%H:%M:%SZ", time.gmtime(epoch)).strip()
@@ -181,6 +180,14 @@ class MorituriLogger(result.Logger):
         # Peak level
         peak = trackResult.peak
         lines.append("    Peak level: %.6f" % peak)
+
+        # Pre-emphasis status
+        # Only implemented in whipper (t.pre_emphasis)
+        if t.pre_emphasis:
+            preEmph = "Yes"
+        else:
+            preEmph = "No"
+        lines.append("    Pre-emphasis: %s" % preEmph)
 
         # Extraction speed
         if trackResult.copyspeed:

commit 0e395e5f0e35a29963451a51f0aeb46564f49bd3

Fix bug introduced in fb271f08c

morituri/command/cd.py
@@ -392,6 +392,8 @@ Log files will log the path to tracks relative to this directory.
             if number > 0:
                 trackResult.pregap = self.itable.tracks[number - 1].getPregap()
 
+                trackResult.pre_emphasis = self.itable.tracks[number - 1].pre_emphasis
+
             # FIXME: optionally allow overriding reripping
             if os.path.exists(path):
                 if path != trackResult.filename:

morituri/result/logger.py
@@ -182,8 +182,8 @@ class MorituriLogger(result.Logger):
         lines.append("    Peak level: %.6f" % peak)
 
         # Pre-emphasis status
-        # Only implemented in whipper (t.pre_emphasis)
-        if t.pre_emphasis:
+        # Only implemented in whipper (trackResult.pre_emphasis)
+        if trackResult.pre_emphasis:
             preEmph = "Yes"
         else:
             preEmph = "No"

morituri/result/result.py
@@ -57,6 +57,7 @@ class TrackResult:
     number = None
     filename = None
     pregap = 0 # in frames
+    pre_emphasis = None
 
     peak = 0.0
     quality = 0.0