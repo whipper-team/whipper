a8af9b79abd03fb9060ca10d964ce2eb8bd2b8d3
Frederik “Freso” S. Olesen <freso.dk@gmail.com>
2017-04-26 16:09:13 +0200

    s/morituri/whipper/ in GPL copyright blurbs.

--- morituri/common/encode.py
+++ morituri/common/encode.py
@@ -5,20 +5,20 @@
 
 # Copyright (C) 2009 Thomas Vander Stichele
 
-# This file is part of morituri.
+# This file is part of whipper.
 #
-# morituri is free software: you can redistribute it and/or modify
+# whipper is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# morituri is distributed in the hope that it will be useful,
+# whipper is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with morituri.  If not, see <http://www.gnu.org/licenses/>.
+# along with whipper.  If not, see <http://www.gnu.org/licenses/>.
 
 
 from mutagen.flac import FLAC
