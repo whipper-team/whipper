af18471cbaded57337809e42650ee8bc44475f82
JoeLametta <JoeLametta@users.noreply.github.com>
2017-06-30 18:17:29 +0200

    Fix failing unittest in systemd-nspawn
    Fixes #157
    Only test_common_directory.py is changed because the other unittest is working as expected (the failure isn't related to whipper).

--- whipper/test/test_common_directory.py
+++ whipper/test/test_common_directory.py
@@ -1,16 +1,18 @@
 # -*- Mode: Python; test-case-name: whipper.test.test_common_directory -*-
 # vi:si:et:sw=4:sts=4:ts=4
 
+from os.path import dirname, expanduser
 from whipper.common import directory
-
 from whipper.test import common
 
 
 class DirectoryTestCase(common.TestCase):
+    HOME = expanduser('~')
+    HOME_PARENT = dirname(HOME)
 
     def testAll(self):
         path = directory.config_path()
-        self.failUnless(path.startswith('/home'))
+        self.failUnless(path.startswith(DirectoryTestCase.HOME_PARENT))
 
         path = directory.cache_path()
-        self.failUnless(path.startswith('/home'))
+        self.failUnless(path.startswith(DirectoryTestCase.HOME_PARENT))
