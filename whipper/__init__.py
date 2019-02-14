af748c55b7dce0db3d74cff9f20f63cd6f7d5df4
Frederik “Freso” S. Olesen <freso.dk@gmail.com>
2019-02-14 11:25:22 +0100

    Use git to get whipper's version
    This essentially a copy of code from spotify2musicbrainz:
    https://gitlab.com/Freso/spotify2musicbrainz/commit/59157165c4ca4e7bb1d5d8b081365029ddf55c72
    
    Given that that code is in GPLv3 (and I'm its author :)), it should be
    fine to use. It is mostly boilerplate from upstream documentation
    anyway: https://pypi.org/project/setuptools-scm/
    
    Should fix https://github.com/whipper-team/whipper/issues/337
    
    Signed-off-by: Frederik “Freso” S. Olesen <freso.dk@gmail.com>

--- whipper/__init__.py
+++ whipper/__init__.py
@@ -2,7 +2,14 @@ import logging
 import os
 import sys
 
-__version__ = '0.7.3'
+from pkg_resources import (get_distribution,
+                           DistributionNotFound, RequirementParseError)
+try:
+    __version__ = get_distribution(__name__).version
+except (DistributionNotFound, RequirementParseError):
+    # not installed as package or is being run from source/git checkout
+    from setuptools_scm import get_version
+    __version__ = get_version()
 
 level = logging.INFO
 if 'WHIPPER_DEBUG' in os.environ:
