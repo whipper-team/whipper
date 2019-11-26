whipper-team/whipper

commit 64dd9d843ab508651863fa30c73ab10a5b1e97f5

Initial pass on python 3 port
Given the imminent end-of-life for Python 2, I didn't bother making the
codebase compatible with both.

Signed-off-by: Drew DeVault <sir@cmpwn.com>

whipper/common/accurip.py
@@ -60,7 +60,7 @@ class _AccurateRipResponse(object):
         position, so track 1 will have array index 0, track 2 will have array
         index 1, and so forth. HTOA and other hidden tracks are not included.
         """
-        self.num_tracks = struct.unpack("B", data[0])[0]
+        self.num_tracks = data[0]
         self.discId1 = "%08x" % struct.unpack("<L", data[1:5])[0]
         self.discId2 = "%08x" % struct.unpack("<L", data[5:9])[0]
         self.cddbDiscId = "%08x" % struct.unpack("<L", data[9:13])[0]
@@ -69,7 +69,7 @@ class _AccurateRipResponse(object):
         self.checksums = []
         pos = 13
         for _ in range(self.num_tracks):
-            confidence = struct.unpack("B", data[pos])[0]
+            confidence = data[pos]
             checksum = "%08x" % struct.unpack("<L", data[pos + 1:pos + 5])[0]
             self.confidences.append(confidence)
             self.checksums.append(checksum)
@@ -88,7 +88,7 @@ class _AccurateRipResponse(object):
 def _split_responses(raw_entry):
     responses = []
     while raw_entry:
-        track_count = struct.unpack("B", raw_entry[0])[0]
+        track_count = raw_entry[0]
         nbytes = 1 + 12 + track_count * (1 + 8)
         responses.append(_AccurateRipResponse(raw_entry[:nbytes]))
         raw_entry = raw_entry[nbytes:]

whipper/common/cache.py
@@ -124,11 +124,7 @@ class PersistedCache:
 
     def __init__(self, path):
         self.path = path
-        try:
-            os.makedirs(self.path)
-        except OSError as e:
-            if e.errno != os.errno.EEXIST:  # FIXME: errno 17 is 'File Exists'
-                raise
+        os.makedirs(self.path, exist_ok=True)
 
     def _getPath(self, key):
         return os.path.join(self.path, '%s.pickle' % key)

whipper/common/config.py
@@ -18,13 +18,12 @@
 # You should have received a copy of the GNU General Public License
 # along with whipper.  If not, see <http://www.gnu.org/licenses/>.
 
-import ConfigParser
 import codecs
+import configparser
 import os.path
 import shutil
 import tempfile
-import urllib
-from urlparse import urlparse
+from urllib.parse import urlparse, quote
 
 from whipper.common import directory
 
@@ -37,7 +36,7 @@ class Config:
     def __init__(self, path=None):
         self._path = path or directory.config_path()
 
-        self._parser = ConfigParser.SafeConfigParser()
+        self._parser = configparser.ConfigParser()
 
         self.open()
 
@@ -51,7 +50,7 @@ class Config:
                      len(self._parser.sections()))
 
     def write(self):
-        fd, path = tempfile.mkstemp(suffix=u'.whipperrc')
+        fd, path = tempfile.mkstemp(suffix='.whipperrc')
         handle = os.fdopen(fd, 'w')
         self._parser.write(handle)
         handle.close()
@@ -64,7 +63,7 @@ class Config:
         method = getattr(self._parser, methodName)
         try:
             return method(section, option)
-        except (ConfigParser.NoSectionError, ConfigParser.NoOptionError):
+        except (configparser.NoSectionError, configparser.NoOptionError):
             return None
 
     def get(self, section, option):
@@ -102,7 +101,7 @@ class Config:
 
         try:
             return int(self._parser.get(section, 'read_offset'))
-        except ConfigParser.NoOptionError:
+        except configparser.NoOptionError:
             raise KeyError("Could not find read_offset for %s/%s/%s" % (
                 vendor, model, release))
 
@@ -121,7 +120,7 @@ class Config:
 
         try:
             return self._parser.get(section, 'defeats_cache') == 'True'
-        except ConfigParser.NoOptionError:
+        except configparser.NoOptionError:
             raise KeyError("Could not find defeats_cache for %s/%s/%s" % (
                 vendor, model, release))
 
@@ -153,7 +152,7 @@ class Config:
         try:
             section = self._findDriveSection(vendor, model, release)
         except KeyError:
-            section = 'drive:' + urllib.quote('%s:%s:%s' % (
+            section = 'drive:' + quote('%s:%s:%s' % (
                 vendor, model, release))
             self._parser.add_section(section)
             for key in ['vendor', 'model', 'release']:

whipper/image/table.py
@@ -23,8 +23,7 @@ Wrap Table of Contents.
 """
 
 import copy
-import urllib
-import urlparse
+from urllib.parse import urlunparse, urlencode
 
 import whipper
 
@@ -66,7 +65,7 @@ class Track:
     :vartype isrc:         str
     :cvar cdtext:          dictionary of CD Text information;
                            :any:`see CDTEXT_KEYS`
-    :vartype cdtext:       str -> unicode
+    :vartype cdtext:       str
     :cvar pre_emphasis:    whether track is pre-emphasised
     :vartype pre_emphasis: bool
     """
@@ -91,10 +90,10 @@ class Track:
     def index(self, number, absolute=None, path=None, relative=None,
               counter=None):
         """
-        :type path:  unicode or None
+        :type path:  str or None
         """
         if path is not None:
-            assert isinstance(path, unicode), "%r is not unicode" % path
+            assert isinstance(path, str), "%r is not str" % path
 
         i = Index(number, absolute, path, relative, counter)
         self.indexes[number] = i
@@ -133,7 +132,7 @@ class Index:
     """
     :cvar counter: counter for the index source; distinguishes between
                    the matching FILE lines in .cue files for example
-    :vartype path:    unicode or None
+    :vartype path:    str or None
     """
     number = None
     absolute = None
@@ -145,7 +144,7 @@ class Index:
                  counter=None):
 
         if path is not None:
-            assert isinstance(path, unicode), "%r is not unicode" % path
+            assert isinstance(path, str), "%r is not str" % path
 
         self.number = number
         self.absolute = absolute
@@ -221,9 +220,10 @@ class Table(object):
             # if on a session border, subtract the session leadin
             thisTrack = self.tracks[number - 1]
             nextTrack = self.tracks[number]
-            if nextTrack.session > thisTrack.session:
-                gap = self._getSessionGap(nextTrack.session)
-                end -= gap
+            if None not in [thisTrack.session, nextTrack.session]:
+                if nextTrack.session > thisTrack.session:
+                    gap = self._getSessionGap(nextTrack.session)
+                    end -= gap
 
         return end
 
@@ -297,8 +297,8 @@ class Table(object):
         # FIXME: we can't replace these calculations with the getFrameLength
         # call because the start and leadout in the algorithm get rounded
         # before making the difference
-        startSeconds = self.getTrackStart(1) / common.FRAMES_PER_SECOND
-        leadoutSeconds = leadout / common.FRAMES_PER_SECOND
+        startSeconds = self.getTrackStart(1) // common.FRAMES_PER_SECOND
+        leadoutSeconds = leadout // common.FRAMES_PER_SECOND
         t = leadoutSeconds - startSeconds
         # durationFrames = self.getFrameLength(data=True)
         # duration = durationFrames / common.FRAMES_PER_SECOND
@@ -348,12 +348,12 @@ class Table(object):
         sha = sha1()
 
         # number of first track
-        sha.update("%02X" % values[0])
+        sha.update(("%02X" % values[0]).encode())
 
         # number of last track
-        sha.update("%02X" % values[1])
+        sha.update(("%02X" % values[1]).encode())
 
-        sha.update("%08X" % values[2])
+        sha.update(("%08X" % values[2]).encode())
 
         # offsets of tracks
         for i in range(1, 100):
@@ -361,7 +361,7 @@ class Table(object):
                 offset = values[2 + i]
             except IndexError:
                 offset = 0
-            sha.update("%08X" % offset)
+            sha.update(("%08X" % offset).encode())
 
         digest = sha.digest()
         assert len(digest) == 20, \
@@ -372,10 +372,10 @@ class Table(object):
         # (Rob) used ., _, and -
 
         # base64 altchars specify replacements for + and /
-        result = base64.b64encode(digest, '._')
+        result = base64.b64encode(digest, b'._').decode()
 
         # now replace =
-        result = "-".join(result.split("="))
+        result = result.replace("=", "-")
         assert len(result) == 28, \
             "Result should be 28 characters, not %d" % len(result)
 
@@ -389,13 +389,13 @@ class Table(object):
         discid = self.getMusicBrainzDiscId()
         values = self._getMusicBrainzValues()
 
-        query = urllib.urlencode({
+        query = urlencode({
             'id': discid,
             'toc': ' '.join([str(v) for v in values]),
             'tracks': self.getAudioTracks(),
         })
 
-        return urlparse.urlunparse((
+        return urlunparse((
             'https', host, '/cdtoc/attach', '', query, ''))
 
     def getFrameLength(self, data=False):
@@ -477,7 +477,7 @@ class Table(object):
 
         Dump our internal representation to a .cue file content.
 
-        :rtype: unicode
+        :rtype: str
         """
         logger.debug('generating .cue for cuePath %r', cuePath)
 

whipper/test/common.py
@@ -83,7 +83,7 @@ class UnicodeTestMixin:
     # A helper mixin to skip tests if we're not in a UTF-8 locale
 
     try:
-        os.stat(u'whipper.test.B\xeate Noire.empty')
+        os.stat('whipper.test.B\xeate Noire.empty')
     except UnicodeEncodeError:
         skip = 'No UTF-8 locale'
     except OSError:

whipper/test/test_command_mblookup.py
@@ -1,5 +1,5 @@
 # vi:si:et:sw=4:sts=4:ts=4:set fileencoding=utf-8
-u"""Tests for whipper.command.mblookup"""
+"""Tests for whipper.command.mblookup"""
 
 import os
 import pickle
@@ -9,22 +9,22 @@ from whipper.command import mblookup
 
 
 class MBLookupTestCase(unittest.TestCase):
-    u"""Test cases for whipper.command.mblookup.MBLookup"""
+    """Test cases for whipper.command.mblookup.MBLookup"""
 
     @staticmethod
     def _mock_musicbrainz(discid, country=None, record=False):
-        u"""Mock function for whipper.common.mbngs.musicbrainz function."""
-        filename = u"whipper.discid.{}.pickle".format(discid)
+        """Mock function for whipper.common.mbngs.musicbrainz function."""
+        filename = "whipper.discid.{}.pickle".format(discid)
         path = os.path.join(os.path.dirname(__file__), filename)
         with open(path) as p:
             return pickle.load(p)
 
     def testMissingReleaseType(self):
-        u"""Test that lookup for release without a type set doesn't fail."""
+        """Test that lookup for release without a type set doesn't fail."""
         # Using: Gustafsson, Österberg & Cowle - What's Up? 8 (disc 4)
         # https://musicbrainz.org/release/d8e6153a-2c47-4804-9d73-0aac1081c3b1
         mblookup.musicbrainz = self._mock_musicbrainz
-        discid = u"xu338_M8WukSRi0J.KTlDoflB8Y-"
+        discid = "xu338_M8WukSRi0J.KTlDoflB8Y-"
         # https://musicbrainz.org/cdtoc/xu338_M8WukSRi0J.KTlDoflB8Y-
-        lookup = mblookup.MBLookup([discid], u'whipper mblookup', None)
+        lookup = mblookup.MBLookup([discid], 'whipper mblookup', None)
         lookup.do()

commit fff3014e159e836b2bb41c48e976bb3beb9ec9ab

Address test failures
More details about the fix to the testDuration failure (regression):

```
FAIL: testDuration (whipper.test.test_image_toc.CapitalMergeTestCase)
testDuration
----------------------------------------------------------------------
Traceback (most recent call last):
  File "/home/travis/virtualenv/python3.5.6/lib/python3.5/site-packages/twisted/internet/defer.py", line 151, in maybeDeferred
    result = f(*args, **kw)
  File "/home/travis/virtualenv/python3.5.6/lib/python3.5/site-packages/twisted/internet/utils.py", line 221, in runWithWarningsSuppressed
    reraise(exc_info[1], exc_info[2])
  File "/home/travis/virtualenv/python3.5.6/lib/python3.5/site-packages/twisted/python/compat.py", line 464, in reraise
    raise exception.with_traceback(traceback)
  File "/home/travis/virtualenv/python3.5.6/lib/python3.5/site-packages/twisted/internet/utils.py", line 217, in runWithWarningsSuppressed
    result = f(*a, **kw)
  File "/home/travis/build/whipper-team/whipper/whipper/test/test_image_toc.py", line 271, in testDuration
    self.assertEqual(self.table.getFrameLength(), 173530)
  File "/home/travis/virtualenv/python3.5.6/lib/python3.5/site-packages/twisted/trial/_synctest.py", line 432, in assertEqual
    super(_Assertions, self).assertEqual(first, second, msg)
  File "/opt/python/3.5.6/lib/python3.5/unittest/case.py", line 829, in assertEqual
    assertion_func(first, second, msg=msg)
  File "/opt/python/3.5.6/lib/python3.5/unittest/case.py", line 822, in _baseAssertEqual
    raise self.failureException(msg)
twisted.trial.unittest.FailTest: 184930 != 173530
```

The test fails because if either nextTrack.session or thisTrack.session are None the if is false and the instructions inside it aren't executed. The check for None is needed because Python 3 doesn't allow NoneType comparisons (in Python 2 that was possible).
IIRC correctly in that test nextTrack.session has value 2 while thisTrack.session is None. That means the Python 2 version evaluates the if condition to true, while the Python 3 version in the first commit does not.
With this change both of the values of nextTrack.session and thisTrack.session are compared as int (if None, the value 1 is used for the comparison - as in disc session 1).

Regression introduced in 64dd9d843ab508651863fa30c73ab10a5b1e97f5.

whipper/common/accurip.py
@@ -193,7 +193,8 @@ def _match_responses(tracks, responses):
         for i, track in enumerate(tracks):
             for v in ('v1', 'v2'):
                 if track.AR[v]['CRC'] == r.checksums[i]:
-                    if r.confidences[i] > track.AR[v]['DBConfidence']:
+                    if (track.AR[v]['DBConfidence'] is None or
+                            r.confidences[i] > track.AR[v]['DBConfidence']):
                         track.AR[v]['DBCRC'] = r.checksums[i]
                         track.AR[v]['DBConfidence'] = r.confidences[i]
                         logger.debug(
@@ -242,7 +243,8 @@ def print_report(result):
                     track.AR['v2']['DBCRC']
                 ) if _f])
             max_conf = max(
-                [track.AR[v]['DBConfidence'] for v in ('v1', 'v2')]
+                [track.AR[v]['DBConfidence'] for v in ('v1', 'v2')
+                 if track.AR[v]['DBConfidence'] is not None], default=None
             )
             if max_conf:
                 if max_conf < track.AR['DBMaxConfidence']:

whipper/common/cache.py
@@ -98,7 +98,7 @@ class Persister:
         if not os.path.exists(self._path):
             return
 
-        handle = open(self._path)
+        handle = open(self._path, 'rb')
         import pickle
 
         try:

whipper/common/config.py
@@ -44,7 +44,7 @@ class Config:
         # Open the file with the correct encoding
         if os.path.exists(self._path):
             with codecs.open(self._path, 'r', encoding='utf-8') as f:
-                self._parser.readfp(f)
+                self._parser.read_file(f)
 
         logger.debug('loaded %d sections from config file',
                      len(self._parser.sections()))

whipper/common/renamer.py
@@ -91,7 +91,7 @@ class Operator:
         Execute the operations
         """
 
-    def next(self):
+    def __next__(self):
         operation = self._todo[len(self._done)]
         if self._resuming:
             operation.redo()
@@ -199,7 +199,8 @@ class RenameInFile(Operation):
             (fd, name) = tempfile.mkstemp(suffix='.whipper')
 
             for s in handle:
-                os.write(fd, s.replace(self._source, self._destination))
+                os.write(fd,
+                         s.replace(self._source, self._destination).encode())
 
             os.close(fd)
             os.rename(name, self._path)

whipper/image/table.py
@@ -220,10 +220,12 @@ class Table:
             # if on a session border, subtract the session leadin
             thisTrack = self.tracks[number - 1]
             nextTrack = self.tracks[number]
-            if None not in [thisTrack.session, nextTrack.session]:
-                if nextTrack.session > thisTrack.session:
-                    gap = self._getSessionGap(nextTrack.session)
-                    end -= gap
+            # The session attribute of a track is None by default (session 1)
+            # with value > 1 if the track is in another session. Py3 doesn't
+            # allow NoneType comparisons so we compare against 1 in that case
+            if int(nextTrack.session or 1) > int(thisTrack.session or 1):
+                gap = self._getSessionGap(nextTrack.session)
+                end -= gap
 
         return end
 
@@ -286,7 +288,7 @@ class Table:
             offset = self.getTrackStart(track.number) + delta
             offsets.append(offset)
             debug.append(str(offset))
-            seconds = offset / common.FRAMES_PER_SECOND
+            seconds = offset // common.FRAMES_PER_SECOND
             n += self._cddbSum(seconds)
 
         # the 'real' leadout, not offset by 150 frames
@@ -389,11 +391,11 @@ class Table:
         discid = self.getMusicBrainzDiscId()
         values = self._getMusicBrainzValues()
 
-        query = urlencode({
-            'id': discid,
-            'toc': ' '.join([str(v) for v in values]),
-            'tracks': self.getAudioTracks(),
-        })
+        query = urlencode([
+            ('toc', ' '.join([str(v) for v in values])),
+            ('tracks', self.getAudioTracks()),
+            ('id', discid),
+        ])
 
         return urlunparse((
             'https', host, '/cdtoc/attach', '', query, ''))

whipper/test/common.py
@@ -70,7 +70,7 @@ class TestCase(unittest.TestCase):
         version so we can use it in comparisons.
         """
         cuefile = os.path.join(os.path.dirname(__file__), name)
-        ret = open(cuefile).read().decode('utf-8')
+        ret = open(cuefile).read()
         ret = re.sub(
             'REM COMMENT "whipper.*',
             'REM COMMENT "whipper %s"' % whipper.__version__,

whipper/test/test_command_mblookup.py
@@ -16,7 +16,7 @@ class MBLookupTestCase(unittest.TestCase):
         """Mock function for whipper.common.mbngs.musicbrainz function."""
         filename = "whipper.discid.{}.pickle".format(discid)
         path = os.path.join(os.path.dirname(__file__), filename)
-        with open(path) as p:
+        with open(path, "rb") as p:
             return pickle.load(p)
 
     def testMissingReleaseType(self):

whipper/test/test_common_accurip.py
@@ -2,7 +2,7 @@
 # vi:si:et:sw=4:sts=4:ts=4
 
 import sys
-from StringIO import StringIO
+from io import StringIO
 from os import chmod, makedirs
 from os.path import dirname, exists, join
 from shutil import copy, rmtree
@@ -22,7 +22,7 @@ class TestAccurateRipResponse(TestCase):
     def setUpClass(cls):
         cls.path = 'c/1/2/dBAR-002-0000f21c-00027ef8-05021002.bin'
         cls.entry = _split_responses(
-            open(join(dirname(__file__), cls.path[6:])).read()
+            open(join(dirname(__file__), cls.path[6:]), "rb").read()
         )
         cls.other_path = '4/8/2/dBAR-011-0010e284-009228a3-9809ff0b.bin'
 
@@ -101,7 +101,7 @@ class TestVerifyResult(TestCase):
     def setUpClass(cls):
         path = 'c/1/2/dBAR-002-0000f21c-00027ef8-05021002.bin'
         cls.responses = _split_responses(
-            open(join(dirname(__file__), path[6:])).read()
+            open(join(dirname(__file__), path[6:]), "rb").read()
         )
         cls.checksums = {
             'v1': ['284fc705', '9cc1f32e'],

whipper/test/test_common_mbngs.py
@@ -18,7 +18,7 @@ class MetadataTestCase(unittest.TestCase):
         filename = 'whipper.release.c56ff16e-1d81-47de-926f-ba22891bd2bd.json'
         path = os.path.join(os.path.dirname(__file__), filename)
         handle = open(path, "rb")
-        response = json.loads(handle.read())
+        response = json.loads(handle.read().decode('utf-8'))
         handle.close()
         discid = "b.yqPuCBdsV5hrzDvYrw52iK_jE-"
 
@@ -31,7 +31,7 @@ class MetadataTestCase(unittest.TestCase):
         filename = 'whipper.release.a76714e0-32b1-4ed4-b28e-f86d99642193.json'
         path = os.path.join(os.path.dirname(__file__), filename)
         handle = open(path, "rb")
-        response = json.loads(handle.read())
+        response = json.loads(handle.read().decode('utf-8'))
         handle.close()
         discid = "f7XO36a7n1LCCskkCiulReWbwZA-"
 
@@ -59,7 +59,7 @@ class MetadataTestCase(unittest.TestCase):
         filename = 'whipper.release.e32ae79a-336e-4d33-945c-8c5e8206dbd3.json'
         path = os.path.join(os.path.dirname(__file__), filename)
         handle = open(path, "rb")
-        response = json.loads(handle.read())
+        response = json.loads(handle.read().decode('utf-8'))
         handle.close()
         discid = "xAq8L4ELMW14.6wI6tt7QAcxiDI-"
 
@@ -93,7 +93,7 @@ class MetadataTestCase(unittest.TestCase):
         filename = 'whipper.release.61c6fd9b-18f8-4a45-963a-ba3c5d990cae.json'
         path = os.path.join(os.path.dirname(__file__), filename)
         handle = open(path, "rb")
-        response = json.loads(handle.read())
+        response = json.loads(handle.read().decode('utf-8'))
         handle.close()
         discid = "u0aKVpO.59JBy6eQRX2vYcoqQZ0-"
 
@@ -130,7 +130,7 @@ class MetadataTestCase(unittest.TestCase):
         filename = 'whipper.release.8478d4da-0cda-4e46-ae8c-1eeacfa5cf37.json'
         path = os.path.join(os.path.dirname(__file__), filename)
         handle = open(path, "rb")
-        response = json.loads(handle.read())
+        response = json.loads(handle.read().decode('utf-8'))
         handle.close()
         discid = "RhrwgVb0hZNkabQCw1dZIhdbMFg-"
 
@@ -167,7 +167,7 @@ class MetadataTestCase(unittest.TestCase):
         filename = 'whipper.release.f484a9fc-db21-4106-9408-bcd105c90047.json'
         path = os.path.join(os.path.dirname(__file__), filename)
         handle = open(path, "rb")
-        response = json.loads(handle.read())
+        response = json.loads(handle.read().decode('utf-8'))
         handle.close()
         discid = "X2c2IQ5vUy5x6Jh7Xi_DGHtA1X8-"
 
@@ -214,7 +214,7 @@ class MetadataTestCase(unittest.TestCase):
         filename = 'whipper.release.d8e6153a-2c47-4804-9d73-0aac1081c3b1.json'
         path = os.path.join(os.path.dirname(__file__), filename)
         handle = open(path, "rb")
-        response = json.loads(handle.read())
+        response = json.loads(handle.read().decode('utf-8'))
         handle.close()
         discid = "xu338_M8WukSRi0J.KTlDoflB8Y-"  # disc 4
 
@@ -228,7 +228,7 @@ class MetadataTestCase(unittest.TestCase):
         filename = 'whipper.release.6109ceed-7e21-490b-b5ad-3a66b4e4cfbb.json'
         path = os.path.join(os.path.dirname(__file__), filename)
         handle = open(path, "rb")
-        response = json.loads(handle.read())
+        response = json.loads(handle.read().decode('utf-8'))
         handle.close()
         discid = "cHW1Uutl_kyWNaLJsLmTGTe4rnE-"
 

whipper/test/test_common_renamer.py
@@ -13,7 +13,7 @@ class RenameInFileTestcase(unittest.TestCase):
 
     def setUp(self):
         (fd, self._path) = tempfile.mkstemp(suffix='.whipper.renamer.infile')
-        os.write(fd, 'This is a test\nThis is another\n')
+        os.write(fd, 'This is a test\nThis is another\n'.encode())
         os.close(fd)
 
     def testVerify(self):
@@ -43,7 +43,7 @@ class RenameFileTestcase(unittest.TestCase):
 
     def setUp(self):
         (fd, self._source) = tempfile.mkstemp(suffix='.whipper.renamer.file')
-        os.write(fd, 'This is a test\nThis is another\n')
+        os.write(fd, 'This is a test\nThis is another\n'.encode())
         os.close(fd)
         (fd, self._destination) = tempfile.mkstemp(
             suffix='.whipper.renamer.file')
@@ -87,7 +87,7 @@ class OperatorTestCase(unittest.TestCase):
 
         (fd, self._source) = tempfile.mkstemp(
             suffix='.whipper.renamer.operator')
-        os.write(fd, 'This is a test\nThis is another\n')
+        os.write(fd, 'This is a test\nThis is another\n'.encode())
         os.close(fd)
         (fd, self._destination) = tempfile.mkstemp(
             suffix='.whipper.renamer.operator')

whipper/test/test_image_toc.py
@@ -364,7 +364,7 @@ class StrokesTestCase(common.TestCase):
         ref = self._filterCue(
             open(os.path.join(
                 os.path.dirname(__file__),
-                'strokes-someday.eac.cue')).read()).decode('utf-8')
+                'strokes-someday.eac.cue')).read())
         common.diffStrings(ref, cue)
 
     @staticmethod