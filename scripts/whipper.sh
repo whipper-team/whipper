30ac6ccc63efafb8031385f3315a23e84d24ee64
JoeLametta <JoeLametta@users.noreply.github.com>
2019-12-03 11:39:55 +0000

    Change shebang to sh + shellcheck fixes
    Signed-off-by: JoeLametta <JoeLametta@users.noreply.github.com>

--- scripts/whipper.sh
+++ scripts/whipper.sh
@@ -1,10 +1,10 @@
-#!/usr/bin/env bash
+#!/bin/sh
 CD_DEVICE="/dev/cdrom"
 OUTPUT_DIR="${HOME}/Music"
 
 WRAPPER_CONFIG_FILE="${HOME}/.config/whipper_wrapper"
 if [ -e "${WRAPPER_CONFIG_FILE}" ] ; then
-  . ${WRAPPER_CONFIG_FILE}
+  . "${WRAPPER_CONFIG_FILE}"
 fi
 
 if [ ! -e "${CD_DEVICE}" ] ; then
@@ -15,8 +15,7 @@ fi
 PERSONAL_CONF_DIR="${HOME}/.config/whipper"
 if [ ! -d "${PERSONAL_CONF_DIR}" ] ; then
   echo "Creating ${PERSONAL_CONF_DIR} directory"
-  mkdir -p "${PERSONAL_CONF_DIR}"
-  if [ $? -ne 0 ] ; then
+  if ! mkdir -p "${PERSONAL_CONF_DIR}" ; then
     echo "Failed to create ${PERSONAL_CONF_DIR}" 1>&2
     exit 2
   fi
@@ -26,4 +25,4 @@ docker run -ti --rm \
   --device="${CD_DEVICE}" \
   -v "${PERSONAL_CONF_DIR}":/home/worker/.config/whipper \
   -v "${OUTPUT_DIR}":/home/worker/output \
-  whipperteam/whipper ${@}
+  whipperteam/whipper "${@}"
