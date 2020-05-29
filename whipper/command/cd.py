8802c5482edf150a94ce430d74ad3239ce803780
JoeLametta <JoeLametta@users.noreply.github.com>
2020-05-29 16:24:30 +0000

    Improve error message for unconfigured drive offset
    Fixes #478.
    
    Signed-off-by: JoeLametta <JoeLametta@users.noreply.github.com>

--- whipper/command/cd.py
+++ whipper/command/cd.py
@@ -317,12 +317,12 @@ Log files will log the path to tracks relative to this directory.
         validate_template(self.options.disc_template, 'disc')
 
         if self.options.offset is None:
-            raise ValueError("Drive offset is unconfigured.\n"
-                             "Please install pycdio and run 'whipper offset "
-                             "find' to detect your drive's offset or set it "
-                             "manually in the configuration file. It can "
-                             "also be specified at runtime using the "
-                             "'--offset=value' argument")
+            raise SystemExit(
+                "Error: drive offset unconfigured. Please install pycdio and "
+                "run 'whipper offset find' to detect your drive's offset or "
+                "set it manually in the configuration file. It can also be "
+                "specified at runtime using the '--offset=value' argument"
+            )
 
         if self.options.working_directory is not None:
             self.options.working_directory = os.path.expanduser(
