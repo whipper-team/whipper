57a59f2840f41414238b63a374025aeb0ffd7766
Louis-Philippe Véronneau <pollo@debian.org>
2024-05-17 17:44:56 -0400

    Migrate to PEP517-compliant pyproject.toml build system
    Signed-off-by: Louis-Philippe Véronneau <pollo@debian.org>

--- setup.py
+++ setup.py
@@ -1,34 +1,11 @@
-from setuptools import setup, find_packages, Extension
+from setuptools import Extension, setup
 
 setup(
-    name="whipper",
-    use_scm_version=True,
-    description="a secure cd ripper preferring accuracy over speed",
-    author=['Thomas Vander Stichele', 'The Whipper Team'],
-    maintainer=['The Whipper Team'],
-    url='https://github.com/whipper-team/whipper',
-    license='GPL3',
-    python_requires='>=3.6',
-    packages=find_packages(),
-    setup_requires=['setuptools_scm'],
     ext_modules=[
-        Extension('accuraterip',
-                  libraries=['sndfile'],
-                  sources=['src/accuraterip-checksum.c'])
-    ],
-    extras_require={
-        'cover_art': ["pillow"],
-        'color_log': ["coloredlogs"]
-    },
-    entry_points={
-        'console_scripts': [
-            'whipper = whipper.command.main:main'
-         ]
-    },
-    data_files=[
-        ('share/metainfo', ['com.github.whipper_team.Whipper.metainfo.xml']),
-    ],
-    scripts=[
-        'scripts/accuraterip-checksum',
-    ],
+        Extension(
+            name="accuraterip",
+            libraries=['sndfile'],
+            sources=["src/accuraterip-checksum.c"],
+        ),
+    ]
 )
